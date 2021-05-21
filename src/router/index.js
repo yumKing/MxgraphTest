f1901b4b5708328b1d2e7e868dfa787ccf90b64f
yumKing <jy11215288@163.com>
2021-05-21 19:04:31 +0800

    myflow commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,84 +1,78 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
-    path: '/graph',
-    redirect: '/graph/base',
-  },
-  {
-    path: '/graph/base',
-    name: 'Base',
-    component: () =>
-      import(/* webpackChunkName: "gbase" */ '../views/mxgraph/Base.vue'),
-  },
-  {
-    path: '/graph/baseGraph',
-    name: 'BaseGraph',
-    component: () =>
-      import(
-        /* webpackChunkName: "baseGraph" */ '../views/mxgraph/BaseGraph.vue'
-      ),
-  },
-  {
-    path: '/graph/anchors',
-    name: 'Anchors',
-    component: () =>
-      import(/* webpackChunkName: "anchors" */ '../views/mxgraph/Anchors.vue'),
-  },
-  {
-    path: '/graph/animation',
-    name: 'Animation',
-    component: () =>
-      import(
-        /* webpackChunkName: "animation" */ '../views/mxgraph/Animation.vue'
-      ),
-  },
-  {
-    path: '/graph/boundary',
-    name: 'Boundary',
-    component: () =>
-      import(
-        /* webpackChunkName: "boundary" */ '../views/mxgraph/Boundary.vue'
-      ),
-  },
-  {
-    path: '/graph/clipboard',
-    name: 'Clipboard',
-    component: () =>
-      import(
-        /* webpackChunkName: "clipboard" */ '../views/mxgraph/Clipboard.vue'
-      ),
-  },
-  {
-    path: '/graph/codec',
-    name: 'Codec',
-    component: () =>
-      import(/* webpackChunkName: "codec" */ '../views/mxgraph/Codec.vue'),
-  },
-  {
-    path: '/graph/myflow',
-    name: 'MyFlow',
-    component: () =>
-      import(/* webpackChunkName: "myflow" */ '../views/mxgraph/MyFlow.vue'),
-  },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    },
+    {
+        path: '/graph',
+        redirect: '/graph/base',
+    },
+    {
+        path: '/graph/base',
+        name: 'Base',
+        component: () => import(/* webpackChunkName: "gbase" */ '../views/mxgraph/Base.vue'),
+    },
+    {
+        path: '/graph/baseGraph',
+        name: 'BaseGraph',
+        component: () => import(
+        /* webpackChunkName: "baseGraph" */ '../views/mxgraph/BaseGraph.vue'),
+    },
+    {
+        path: '/graph/anchors',
+        name: 'Anchors',
+        component: () => import(/* webpackChunkName: "anchors" */ '../views/mxgraph/Anchors.vue'),
+    },
+    {
+        path: '/graph/animation',
+        name: 'Animation',
+        component: () => import(
+        /* webpackChunkName: "animation" */ '../views/mxgraph/Animation.vue'),
+    },
+    {
+        path: '/graph/boundary',
+        name: 'Boundary',
+        component: () => import(
+        /* webpackChunkName: "boundary" */ '../views/mxgraph/Boundary.vue'),
+    },
+    {
+        path: '/graph/clipboard',
+        name: 'Clipboard',
+        component: () => import(
+        /* webpackChunkName: "clipboard" */ '../views/mxgraph/Clipboard.vue'),
+    },
+    {
+        path: '/graph/edgeDynStyle',
+        name: 'EdgeDynamicStyle',
+        component: () => import(
+        /* webpackChunkName: "edgeDynStyle" */ '../views/mxgraph/EdgeDynamicStyle.vue'),
+    },
+    {
+        path: '/graph/autoLayout',
+        name: 'AutoLayout',
+        component: () => import(
+        /* webpackChunkName: "autoLayout" */ '../views/mxgraph/AutoLayout.vue'),
+    },
+    {
+        path: '/graph/myflow',
+        name: 'MyFlow',
+        component: () => import(/* webpackChunkName: "myflow" */ '../views/mxgraph/MyFlow.vue'),
+    },
 ];
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+    history: createWebHashHistory(),
+    routes,
 });
 export default router;
 //# sourceMappingURL=index.js.map
--- src/views/mxgraph/MyFlow.vue
+++ src/views/mxgraph/MyFlow.vue
@@ -34,19 +34,31 @@ export default defineComponent({
         mi.mxUtils.error('Browser is not supported!', 200, false);
       } else {
         const container = graphContainer.value as HTMLElement;
+        // 不允许内置的上下文菜单
         mi.mxEvent.disableContextMenu(container);
 
         graph = new mi.mxGraph(container);
         // config graph
         // 设置结点样式
         const vertexStyle = graph.getStylesheet().getDefaultVertexStyle();
+        // 边缘为圆角
         vertexStyle[mi.mxConstants.STYLE_ROUNDED] = '1';
+        // 启用结点阴影
         vertexStyle[mi.mxConstants.STYLE_SHADOW] = true;
-        vertexStyle[mi.mxConstants.STYLE_STROKECOLOR] = '#cdcdcd';
+        // 结点边框透明
+        vertexStyle[mi.mxConstants.STYLE_STROKECOLOR] = 'transparent';
+        // 阴影颜色
         mi.mxConstants.SHADOWCOLOR = '#cdcdcd';
+        // 阴影相对结点右偏移
         mi.mxConstants.SHADOW_OFFSET_X = 3;
+        // 阴影相对结点下偏移
         mi.mxConstants.SHADOW_OFFSET_Y = 3;
+        // 阴影透明度
         mi.mxConstants.SHADOW_OPACITY = 0.3;
+        // 结点label样式
+        vertexStyle[mi.mxConstants.STYLE_LABEL_BACKGROUNDCOLOR] = '#ffffff'
+        vertexStyle[mi.mxConstants.STYLE_FONTCOLOR] = 'rgba(0,0,0,.65)'
+        vertexStyle[mi.mxConstants.STYLE_FONTFAMILY] = 'Helvetica Neue,Helvetica,PingFang SC,Hiragino Sans GB,Microsoft YaHei,Arial,sans-serif;'
 
         // 设置 连线样式为曲线
         const edgeStyle = graph.getStylesheet().getDefaultEdgeStyle();
@@ -55,10 +67,20 @@ export default defineComponent({
         edgeStyle[mi.mxConstants.STYLE_CURVED] = '1';
         graph.view.getPoint = (state, geo) => {
           const ponits = createBezierPoints(state.absolutePoints, 5);
-          return new mi.mxPoint(ponits[1].x, ponits[1].y);
+          return new mi.mxPoint(ponits[2].x, ponits[2].y);
         };
         edgeStyle[mi.mxConstants.STYLE_ENDARROW] =
           mi.mxConstants.ARROW_OPEN_THIN;
+        // 连线线条粗细
+        edgeStyle[mi.mxConstants.EDGE_SELECTION_STROKEWIDTH] = 2
+        // 连线线条颜色
+        edgeStyle[mi.mxConstants.EDGE_SELECTION_COLOR] = 'white'
+        // 连线端点与结点的距离
+        edgeStyle[mi.mxConstants.STYLE_PERIMETER_SPACING] = 4
+        // 连线label样式
+        edgeStyle[mi.mxConstants.STYLE_LABEL_BACKGROUNDCOLOR] = '#ffffff'
+        edgeStyle[mi.mxConstants.STYLE_FONTCOLOR] = 'rgb(68,98,153)'
+        edgeStyle[mi.mxConstants.STYLE_FONTSTYLE] = '1'
 
         // 结点不允许连线
         graph.setConnectable(false);
@@ -71,24 +93,16 @@ export default defineComponent({
         // 边和结点上的label不允许移动
         graph.edgeLabelsMovable = false;
         graph.vertexLabelsMovable = false;
+        // 不允许重设结点宽高
+        graph.setCellsResizable(false)
+        // 不允许从父结点中移除cells
+        graph.graphHandler.removeCellsFromParent = false;
 
         new mi.mxKeyHandler(graph);
         new mi.mxRubberband(graph);
 
-        // 不允许从父结点中移除cells
-        graph.graphHandler.removeCellsFromParent = false;
-        // 在添加cell时自动size
-        graph.autoSizeCellsOnAdd = true;
-
-        // 解除cell 锁定， 允许移动 相关的cells
-        graph.isCellLocked = (cell: any): boolean => {
-          return graph.isCellsLocked();
-        };
-        graph.isCellResizable = (cell: any): boolean => {
-          let geo = graph.model.getGeometry(cell);
-          return geo == null || geo.relative;
-        };
-        // 将标签截断为顶点大小
+        
+        // // 将标签截断为顶点大小
         graph.getLabel = (cell) => {
           let label = graph.labelsVisible
             ? graph.convertValueToString(cell)
@@ -114,11 +128,11 @@ export default defineComponent({
           }
           return label;
         };
-        // 使能包装 顶点标签
-        graph.isWrapping = (cell: any) => {
-          return graph.model.isCollapsed(cell);
-        };
-        // 如果没有定义偏移量 使能 剪辑
+        // // 使能包装 顶点标签
+        // graph.isWrapping = (cell: any) => {
+        //   return graph.model.isCollapsed(cell);
+        // };
+        // // 如果没有定义偏移量 使能 剪辑
         graph.isLabelClipped = (cell: mx.mxCell): boolean => {
           let geo = graph.model.getGeometry(cell);
           return (
@@ -128,9 +142,9 @@ export default defineComponent({
           );
         };
 
-        // 设置动态样式改变标记
-        graph.getView().updateStyle = true;
-        // 覆盖 mxGraphModel.getStyle 以返回指定样式(连线结束的结点样式颜色)
+        // // 设置动态样式改变标记
+        // graph.getView().updateStyle = true;
+        // // 覆盖 mxGraphModel.getStyle 以返回指定样式(连线结束的结点样式颜色)
         graph.model.getStyle = (cell: any): string | null => {
           const that = graph.model as any;
           if (cell != null) {
@@ -162,6 +176,39 @@ export default defineComponent({
           }
           return null;
         };
+
+        // 当鼠标移入结点时，高亮结点
+        const highlight = new mi.mxCellTracker(graph, '#00ff00');
+        // 给graph添加一个点击事件， 并判断点击的cell是否有图层，有则删除，无则添加一个图层，并给图层添加一个点击事件
+        graph.addListener(mi.mxEvent.CLICK, (sender, evt) => {
+          const cell = evt.getProperty('cell');
+          if (cell != null) {
+            const overlays = graph.getCellOverlays(cell);
+            if (overlays == null) {
+              // 创建一个图层
+              let overlay = new mi.mxCellOverlay(
+                new mi.mxImage('mxgraph/images/overlays/check.png', 16, 16),
+                'Overlay Tooltip'
+              );
+              // 给图层添加一个点击事件
+              overlay.addListener(mi.mxEvent.CLICK, (sender, evt2) => {
+                mi.mxUtils.alert('overlay clicked');
+              });
+
+              // 将图层添加到cell中
+              graph.addCellOverlay(cell, overlay);
+            } else {
+              graph.removeCellOverlays(cell);
+            }
+          }
+        });
+
+        // 给graph添加双击事件
+        graph.addListener(mi.mxEvent.DOUBLE_CLICK, (sender, evt) => {
+          const cell = evt.getProperty('cell');
+          mi.mxUtils.alert('DoubleClick: ' + (cell != null ? 'Cell' : 'Graph'));
+          evt.consume();
+        });
 
         var parent = graph.getDefaultParent();
 
